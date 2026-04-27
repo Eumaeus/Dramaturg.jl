83698fd4a67a25dee450f15f82284577fa509d66
Eumaeus <cwblackwell@gmail.com>
2026-04-26 21:03:11 -0400

    Adding LSJ index; setting up to align tokens with lexion entries by URN.

--- examples/parse_morpheus.jl
+++ examples/parse_morpheus.jl
@@ -1,19 +1,20 @@
 using Dramaturg.Morphology   # or wherever you include it
 
-#=
+
 parse_morpheus_to_triplets(
     "morph/output/analysis.txt",          # your real output
-    "morph/output/morpheus_triplets.tsv",         # clean triplets you wanted
-    "morph/output/parse_errors.log"
+    "morph/output/frogs-morpheus_triplets.tsv",         # clean triplets you wanted
+    "morph/output/frogs-parse_errors.log"
 )
-=#
+
 
 # Or test it on the hand-curated data:
-
+#=
 parse_morpheus_to_triplets(
     "morph/test-output/analysis.txt",
     "morph/test-output/test-triplets.tsv",
     "morph/test-output/parse_errors.log"
 )
+=#
 
 
